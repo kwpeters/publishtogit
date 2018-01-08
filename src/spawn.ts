14bc9ca747921a91d6927053381fbe75d51c6b4c
Kevin Peters <kwpeters@gmail.com>
2018-01-08 08:43:05 -0500

    Chanded spawn() so that it resolves with trimmed stdout.

--- src/spawn.ts
+++ src/spawn.ts
@@ -3,6 +3,7 @@ import * as cp from "child_process";
 import {Writable} from "stream";
 import {NullStream} from "./nullStream";
 import {eventToPromise} from "./promiseHelpers";
+import * as _ from "lodash";
 
 
 /**
@@ -14,14 +15,15 @@ import {eventToPromise} from "./promiseHelpers";
  * @param args - An array of arguments for cmd
  * @param cwd - The current working directory for the child process
  * @param stdoutStream - The stream to receive stdout.  A NullStream if
- * undefined.
- * For example:
- * `new CombinedStream(new PrefixStream("foo"), process.stdout)`
- * @param stderrStream - The stream to receive stderr  A NullStream if undefined.
- * For example:
- * `new CombinedStream(new PrefixStream(".    "), process.stderr)`
+ *     undefined.
+ *     For example:
+ *     `new CombinedStream(new PrefixStream("foo"), process.stdout)`
+ * @param stderrStream - The stream to receive stderr  A NullStream if
+ *     undefined. For example:
+ *     `new CombinedStream(new PrefixStream(".    "), process.stderr)`
  * @return {Promise<string>} A Promise that is resolved when the child process's
- *     output when the exit code is 0 and is rejected when it is non-zero.
+ *     trimmed output when the exit code is 0 and is rejected when it is
+ *     non-zero.
  */
 export function spawn(
     cmd: string,
@@ -71,7 +73,7 @@ export function spawn(
                     {
                         console.log(`Child process succeeded: ${cmdLineRepresentation}`);
                     }
-                    resolve(stdoutCollector.collected);
+                    resolve(_.trim(stdoutCollector.collected));
                 } else {
                     if (description)
                     {
